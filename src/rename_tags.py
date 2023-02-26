d9f50679e15665ac652affa86519a0c0601fe678
Roy Straub <roy@codecraftr.nl>
2023-02-26 19:57:44 +0100

    Play around with an implementation

--- src/rename_tags.py
+++ src/rename_tags.py
@@ -1,2 +1,12 @@
-def rename_tags(contents, old_tag, new_tag):
-    print("rename tags")
+def rename_tags_in_post(file_path, old_tag, new_tag):
+    # Read the contents of the file
+    with open(file_path, 'r') as file:
+        contents = file.read()
+
+    # Replace the old tag with the new tag
+    contents = contents.replace(old_tag, new_tag)
+
+    # Write the contents back to the file
+    with open(file_path, 'w') as file:
+        file.write(contents)
+
--- tests/test_rename_tags.py
+++ tests/test_rename_tags.py
@@ -1,21 +1,28 @@
 import os
 
-from src.rename_tags import rename_tags
+from src.rename_tags import rename_tags_in_post
 
 
 def test_should_rename_matching_tag_with_new_tag():
-    contents = read_test_blog()
-    print(contents)
-    rename_tags(contents, 'Python', 'Python3')
+    # TODO create a copy of the file
+    contents = test_blog_path()
 
-    assert True is False
+    rename_tags_in_post(contents, 'Python', 'Python3')
+
+    result = test_blog_path()
+
+    assert "Python3" in result
 
 
-def read_test_blog():
+def test_blog_path():
     # Get the directory of the current script
     dir_path = os.path.dirname(os.path.realpath(__file__))
     # Construct the path to the data file
-    test_blog_path = os.path.join(dir_path, 'data/input/test_blog.md')
-    with open(test_blog_path, 'r') as file:
+    return os.path.join(dir_path, 'data/input/test_blog.md')
+
+
+def read_test_blog():
+    with open(test_blog_path(), 'r') as file:
         contents = file.read()
     return contents
+
