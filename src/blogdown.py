389a3ad13181cde663d8bd9e8f5ea76828666586
Roy Straub <roy@codecraftr.nl>
2023-03-06 18:32:46 +0800

    Allow shorthand arguments for the cli

--- src/blogdown.py
+++ src/blogdown.py
@@ -5,9 +5,9 @@ import argparse
 from rename_tags import rename_tags_in_dir
 
 parser = argparse.ArgumentParser(description='Blogdown')
-parser.add_argument("--directory", type=str, help="The directory to process", default=".")
-parser.add_argument("--old-tag", type=str, help="The tag to replace")
-parser.add_argument("--new-tag", type=str, help="The tag that will be used instead")
+parser.add_argument('-d', '--directory', type=str, help='The directory to process', default=".")
+parser.add_argument('-o', '--old-tag', type=str, help='The tag to replace')
+parser.add_argument('-n', '--new-tag', type=str, help='The tag that will be used instead')
 
 args = parser.parse_args()
 
