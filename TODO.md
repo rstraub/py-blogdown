1f7d36a07d5f0861d67c8f91c617d779b16caa91
Roy Straub <roy@codecraftr.nl>
2023-03-06 19:15:39 +0800

    Added main method

--- TODO.md
+++ TODO.md
@@ -3,9 +3,17 @@
 Ideas of features to implement.
 
 - [X] Bulk rename tags
-  - [ ] Rename tags, not content in general
-  - [ ] Only for markdown files
+  - [X] Rename tags, not content in general
 - [ ] Bulk rename categories
 - [ ] Publish blog for jekyll (drafts to posts)
+  - [ ] move file
+  - [ ] prepend date to filename
 - [ ] Extract links and assemble references section
 - [ ] etc...
+
+## General Improvements
+
+- [ ] Only for markdown files
+- [ ] Add logging
+- [ ] Improve error handling
+- [ ] Add more tests
--- src/blogdown.py
+++ src/blogdown.py
@@ -4,11 +4,17 @@ import argparse
 
 from rename_tags import rename_tags_in_dir
 
-parser = argparse.ArgumentParser(description='Blogdown')
-parser.add_argument('-d', '--directory', type=str, help='The directory to process', default=".")
-parser.add_argument('-o', '--old-tag', type=str, help='The tag to replace')
-parser.add_argument('-n', '--new-tag', type=str, help='The tag that will be used instead')
 
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser(description='Streamline your markdown blogging workflow')
+    parser.add_argument('-d', '--directory', type=str, help='Directory to run the rename in', default=".")
+    parser.add_argument('-o', '--old-tag', type=str, required=True, help='Tag to rename')
+    parser.add_argument('-n', '--new-tag', type=str, required=True, help='Replacement tag')
 
-rename_tags_in_dir(args.directory, args.old_tag, args.new_tag)
+    args = parser.parse_args()
+
+    rename_tags_in_dir(args.directory, args.old_tag, args.new_tag)
+
+
+if __name__ == '__main__':
+    main()
