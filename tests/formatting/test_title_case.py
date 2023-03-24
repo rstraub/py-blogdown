840b658d1342107575adaa8feb2123777ef8dbf4
Roy Straub <roy@codecraftr.nl>
2023-03-24 14:33:32 +0100

    Postpone for now

--- tests/formatting/test_title_case.py
+++ tests/formatting/test_title_case.py
@@ -45,3 +45,20 @@ def test_should_not_title_case_capitalized_headings(post: str, expected: str):
 ])
 def test_should_not_title_case_invalid_headings(post: str):
     assert title_case_post(post) == post
+
+
+@pytest.mark.skip(reason='On the roadmap')
+def test_should_title_case_all_headings_in_post():
+    post = """
+    # titlecase is better
+    content
+    ## it really is
+    """
+
+    expected = """
+    # Titlecase is Better
+    content
+    ## It Really Is
+    """
+
+    assert title_case_post(post) == expected
