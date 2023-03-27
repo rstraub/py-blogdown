57184478b4534af693c968003c2ea1cbd73ee725
Roy Straub <roy@codecraftr.nl>
2023-03-27 21:06:52 +0200

    Used the multiline strip utility

--- tests/metadata/test_rename_tag.py
+++ tests/metadata/test_rename_tag.py
@@ -1,23 +1,25 @@
 from src.metadata.rename_tags import rename_tag
+from tests.post_utils import strip_multiline
 
 
 def test_should_rename_matching_tag_with_new_tag():
-    post = """
----
-tags:
-- Test-Driven Development
----
+    post = strip_multiline("""
+    ---
+    tags:
+    - Test-Driven Development
+    ---
+    
+    # Content
+    """)
 
-# Content""".strip()
-
-    expected = """
----
-tags:
-- tdd
----
-
-# Content
-""".strip()
+    expected = strip_multiline("""
+    ---
+    tags:
+    - tdd
+    ---
+    
+    # Content
+    """)
 
     result = rename_tag(post, 'Test-Driven Development', 'tdd')
 
